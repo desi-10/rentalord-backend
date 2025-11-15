f51261e7c088f8cecae3064f7c5f70b2db955e89
desi-10 <desmondkudjuh99@gmail.com>
2025-11-15 08:35:20 +0000

    removed too many request error

--- api/src/features/auth/auth.route.ts
+++ api/src/features/auth/auth.route.ts
@@ -14,49 +14,49 @@ const router = express.Router();
 
 router.post(
   "/generate-otp",
-  createRateLimiter(3, "10 m"), // max 3 per 10 minutes
+  // createRateLimiter(3, "10 m"), // max 3 per 10 minutes
   validateSchema(generateOtpSchema),
   authController.generateOTPController
 );
 
 router.post(
   "/refresh",
-  createRateLimiter(12, "2 m"), // 12 per 2 mins (~1 per 10s)
+  // createRateLimiter(12, "2 m"), // 12 per 2 mins (~1 per 10s)
   authController.refreshTokenController
 );
 
 router.post(
   "/twoFa",
   authenticate,
-  createRateLimiter(5, "5 m"), // 5 per 5 minutes
+  // createRateLimiter(5, "5 m"), // 5 per 5 minutes
   validateSchema(twofaSchema),
   authController.twoFaController
 );
 
 router.post(
   "/register",
-  createRateLimiter(5, "1 m"), // 5 per minute
+  // createRateLimiter(5, "1 m"), // 5 per minute
   validateSchema(registerSchema),
   authController.registerController
 );
 
 router.post(
   "/login",
-  createRateLimiter(5, "1 m"), // 5 per minute
+  // createRateLimiter(5, "1 m"), // 5 per minute
   authController.loginController
 );
 
 router.post(
   "/update-password",
   authenticate,
-  createRateLimiter(5, "10 m"), // 5 per 10 minutes
+  // createRateLimiter(5, "10 m"), // 5 per 10 minutes
   validateSchema(resetPasswordSchema),
   authController.resetPasswordController
 );
 
 router.post(
   "/forgot-password",
-  createRateLimiter(3, "10 m"), // 3 per 10 minutes
+  // createRateLimiter(3, "10 m"), // 3 per 10 minutes
   validateSchema(resetPasswordSchema),
   authController.resetPasswordController
 );
